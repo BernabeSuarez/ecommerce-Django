1b8ebd6ff93f8ce9009c724f4c032496fc6d2f32
Bernabe <bernatattoo@gmail.com>
2023-12-10 01:52:07 -0300

    Add register page

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -37,3 +41,30 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Se ha cerrado su sesion, Muchas Gracias"))
     return redirect("home")
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            # login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(
+                request, ("El usuario se ha creado correctamente... Bienvenido!!!")
+            )
+            return redirect("home")
+        else:
+            messages.success(
+                request,
+                (
+                    "Ha ocurrido un problema al crear el usuario, por favor intente nuevamente..."
+                ),
+            )
+            return redirect("register")
+    else:
+        return render(request, "register.html", {"form": form})
